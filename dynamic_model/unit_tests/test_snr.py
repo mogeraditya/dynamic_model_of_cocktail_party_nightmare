03d0742362039532c21a056348c7b34acee9bd93
aditya_moger <adityabdmi@gmail.com>
2025-11-10 01:07:27 +0100

    Update test_snr.py

--- dynamic_model/unit_tests/test_snr.py
+++ dynamic_model/unit_tests/test_snr.py
@@ -10,11 +10,17 @@ import numpy as np
 sys.path.append("./dynamic_model")
 from agents.bats import Bat
 from agents.sounds import DirectSound
-from supporting_files.utilities import load_parameters
 from supporting_files.snr_implementation import *
+from supporting_files.utilities import load_parameters
 from supporting_files.vectors import Vector
 
 
 class TestingSNR(unittest.TestCase):
     def setUp(self):
-        
+        sound1 = DirectSound(
+            parameters_df=self.parameters_df,
+            origin=self.sound_start_point,
+            creation_time=self.creation_time,
+            emitter_id=self.emitter_id,
+            direction_vector=Vector(1, 0),
+        )
